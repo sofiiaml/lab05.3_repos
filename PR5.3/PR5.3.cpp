49719965eee50b042ae8a7391b06691a8875f0c7
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-26 17:39:54 +0300

    lab5.3

---
+++ PR5.3/PR5.3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double h(const double x);
+int main()
+{
+	double gp, gk, z;
+	int n;
+	
+	cout << "gp = "; cin >> gp;
+	cout << "gk = "; cin >> gk;
+	cout << "n = "; cin >> n;
+	cout << fixed;
+	cout << "------------------------------" << endl;
+	cout << "|" << setw(5) << "g" << "      |"
+		<< setw(12) << "S" << " |"
+		<< endl;
+	cout << "------------------------------" << endl;
+	double dg = (gk - gp) / n;
+	double g = gp;
+	while (g <= gk)
+	{
+		z = h(g * g + 1) + h(g + (h(1))) + 1;
+		cout << "|" << setw(5) << setprecision(2) << g << "      |"
+			<< setw(12) << setprecision(5) << z << " |" << endl;
+		g += dg;
+	}
+	
+	return 0;
+}
+double h(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (1 + cos(x));
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+  			double R = x/(4*pow(i,2)-2*i);
+			a *= R;
+			S += a;
+		} while (i < 5);
+		cout << "|" << setw(6) << setprecision(2) << "g" << "   |"
+			<< setw(12) << setprecision(2) << S << "     |" 
+			<< endl;
+		cout << "------------------------------" << endl;
+		return S;
+	}
+}
---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR5.3/PR5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2);
+			Assert::AreEqual(t, 3.27017, 0.00001);
+		}
+	};
+}
